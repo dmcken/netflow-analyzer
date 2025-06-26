fe9724ed830c6ffcedb22eeae312447f92404c82
David Mc Ken <david.mcken@gmail.com>
2025-06-26 17:06:14 -0400

    Update log output format.

--- goflow2/protobuf-archive/archive_logs.py
+++ goflow2/protobuf-archive/archive_logs.py
@@ -110,5 +110,8 @@ def main() -> None:
     logger.debug("Done")
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    )
     main()
